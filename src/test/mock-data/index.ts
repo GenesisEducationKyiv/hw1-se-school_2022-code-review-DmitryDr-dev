3bfec16c15536557f79def4d4aedecb501ddad1f
DmitryDr-dev <79312440+DmitryDr-dev@users.noreply.github.com>
2022-09-05 20:01:46 +0300

    test: add email & send email result mocks

--- src/test/mock-data/index.ts
+++ src/test/mock-data/index.ts
@@ -1,2 +1,4 @@
 export { exchangeApiResponse } from './exchange-api-response';
 export { exchangeApiRequest } from './exchange-api-request';
+export { subscriptionEmail } from './subscription-email';
+export { sendEmailsResult } from './send-emails-result';
