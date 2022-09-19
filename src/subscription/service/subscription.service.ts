adf7ecf24377684bd1315c0dde7dde0882df0075
DmitryDr-dev <79312440+DmitryDr-dev@users.noreply.github.com>
2022-09-19 23:51:32 +0300

    feat: add subscription repository to service

--- src/subscription/service/subscription.service.ts
+++ src/subscription/service/subscription.service.ts
@@ -1,12 +1,10 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { LocalDbName } from 'src/common/constants';
-import { IExchangeApiService } from 'src/exchange-api/common/service';
-import { ICreatorPool } from 'src/exchange-api/creator-pool';
-import { ICreatorPoolToken } from 'src/exchange-api/exchange-api.module';
-import { ILocalDbServiceToken } from '../../database/local-db/local-db.module';
-import { ILocalDbService } from '../../database/local-db/service';
+import { IExchangeApiService } from '../../exchange-api/common/service';
+import { ICreatorPool } from '../../exchange-api/creator-pool';
+import { ICreatorPoolToken } from '../../exchange-api/exchange-api.module';
 import { IMailServiceToken } from '../../mail/mail.module';
 import { IMailService } from '../../mail/service';
+import { SubscriptionRepository } from '../repository';
 import { ISubscriptionService } from './subscription.service.interface';
 
 @Injectable()
@@ -14,31 +12,32 @@ export class SubscriptionService implements ISubscriptionService {
   private exchangeApi: IExchangeApiService;
 
   constructor(
-    @Inject(ILocalDbServiceToken)
-    private readonly localDbService: ILocalDbService,
     @Inject(ICreatorPoolToken) private readonly creatorTool: ICreatorPool,
     @Inject(IMailServiceToken)
     private readonly mailService: IMailService,
+    private subscriptionRepository: SubscriptionRepository,
   ) {
     this.exchangeApi = this.creatorTool.getExchangeApi();
   }
 
   public async addNewEmail(email: string): Promise<string> {
     try {
-      const result = await this.localDbService.addOne(LocalDbName.Email, email);
+      const result = await this.subscriptionRepository.addOne(email);
+
       return result;
     } catch (error) {
       throw new Error(
-        `Error occurred while creating new contact': ${error.message}`,
+        `Error occurred while creating new contact: ${error.message}`,
       );
     }
   }
 
   public async sendEmails(): Promise<PromiseSettledResult<unknown>[]> {
     try {
-      const emails: Array<string> = await this.localDbService.findAll(
-        LocalDbName.Email,
-      );
+      const emails = await this.subscriptionRepository.findAll();
+
+      if (emails.length === 0) throw new Error('Email list is empty');
+
       const exchangeRate = await this.exchangeApi.getExchangeRateData({
         sourceCurrency: 'BTC',
         targetCurrency: 'UAH',
@@ -60,6 +59,7 @@ export class SubscriptionService implements ISubscriptionService {
       const result: PromiseSettledResult<unknown>[] = await Promise.allSettled(
         allPromises,
       );
+
       return result;
     } catch (error) {
       throw new Error(`Error occurred while sending emails': ${error.message}`);
--- src/subscription/subscription.module.ts
+++ src/subscription/subscription.module.ts
@@ -3,11 +3,12 @@ import { LocalDbModule } from '../database/local-db/local-db.module';
 import { ExchangeApiModule } from '../exchange-api/exchange-api.module';
 import { MailModule } from '../mail/mail.module';
 import { SubscriptionController } from './controller';
+import { SubscriptionRepository } from './repository';
 import { SubscriptionService } from './service';
 
 @Module({
   imports: [LocalDbModule, MailModule, ExchangeApiModule],
-  providers: [SubscriptionService],
+  providers: [SubscriptionService, SubscriptionRepository],
   controllers: [SubscriptionController],
 })
 export class SubscriptionModule {}
