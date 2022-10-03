2b669e2fec6a01e9613cbdddff6f92b586b1c2b8
DmitryDr-dev <79312440+DmitryDr-dev@users.noreply.github.com>
2022-10-03 15:26:06 +0300

    feat: add global exception filter
    - add global exception filter
    - inject logger
    - add message sending on project startup

--- apps/gses2-app/src/main.ts
+++ apps/gses2-app/src/main.ts
@@ -1,15 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { ClientProxy } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { DI_TOKEN } from './common/constants/di-token';
+import { HttpExceptionFilter } from './common/filters';
 import { ValidationPipe } from './common/pipes';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const PORT = process.env.APP_PORT || 3000;
   const app = await NestFactory.create(AppModule);
+  const logger = app.get<ClientProxy>(DI_TOKEN.LoggerRbqToken);
+
   app.setGlobalPrefix('gses2.app/api');
   app.enableCors();
-
+  app.useGlobalFilters(new HttpExceptionFilter(logger));
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(PORT, () => console.info(`Server runs on port: ${PORT}`));
+  await app.listen(PORT, () => {
+    logger.emit('debug', {
+      name: bootstrap.name,
+      timeStamp: new Date().toISOString(),
+      message: `Server runs on port: ${PORT}`,
+      type: 'debug',
+    });
+  });
 }
+
 bootstrap();
