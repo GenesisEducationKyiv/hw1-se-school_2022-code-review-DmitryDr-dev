af0fcb9cbda6b7aa95f770707397cd4e45c6f3c5
DmitryDr-dev <79312440+DmitryDr-dev@users.noreply.github.com>
2022-10-03 15:26:43 +0300

    feat: inject logger module to exception filter

--- apps/gses2-app/src/common/filters/http-exception.filter.ts
+++ apps/gses2-app/src/common/filters/http-exception.filter.ts
@@ -4,15 +4,25 @@ import {
   ExceptionFilter,
   HttpException,
 } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 import { Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  constructor(private readonly loggerClient: ClientProxy) {}
+
   public catch(exception: HttpException, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const status = exception.getStatus();
 
+    this.loggerClient.emit('error', {
+      name: this.constructor.name,
+      timeStamp: new Date().toISOString(),
+      message: exception.message,
+      stack: exception.stack,
+      type: 'error',
+    });
     response.status(status).end();
   }
 }
