d0bff4d15421a44e7c6077ed918c8bf9ea8a706e
DmitryDr-dev <79312440+DmitryDr-dev@users.noreply.github.com>
2022-10-09 16:36:36 +0300

    feat: add logger module to customer app

--- apps/customer-app/src/customer-app.module.ts
+++ apps/customer-app/src/customer-app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    LoggerModule,
   ],
   controllers: [],
   providers: [],
