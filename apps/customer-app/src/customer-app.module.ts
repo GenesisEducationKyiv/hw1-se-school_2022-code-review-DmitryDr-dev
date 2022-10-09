8196cf571b5741b221aa4a0297fddf3a8b3a7607
DmitryDr-dev <79312440+DmitryDr-dev@users.noreply.github.com>
2022-10-09 16:54:43 +0300

    build: add customer controller

--- apps/customer-app/src/customer-app.module.ts
+++ apps/customer-app/src/customer-app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { LoggerModule } from './logger/logger.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { LoggerModule } from './logger/logger.module';
       envFilePath: '.env',
     }),
     LoggerModule,
+    CustomerModule,
   ],
   controllers: [],
   providers: [],
