c6f8a278aca6f1ca94a48e864c9cd63a7c267247
DmitryDr-dev <79312440+DmitryDr-dev@users.noreply.github.com>
2022-10-11 01:06:08 +0300

    build: add di tokens

--- apps/orchestrator-app/src/common/constants/di-token.ts
+++ apps/orchestrator-app/src/common/constants/di-token.ts
@@ -1,3 +1,5 @@
 export const DI_TOKEN = {
   LoggerRbqToken: Symbol.for('LoggerRbqToken'),
+  NotificationRbqToken: Symbol.for('NotificationRbqToken'),
+  CustomerRbqToken: Symbol.for('CustomerRbqToken'),
 };
