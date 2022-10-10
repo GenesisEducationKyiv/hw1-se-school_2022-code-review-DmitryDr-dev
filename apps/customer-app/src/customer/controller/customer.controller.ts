68764cd0a45bf2be00a51c7972948488e0dff0dd
DmitryDr-dev <79312440+DmitryDr-dev@users.noreply.github.com>
2022-10-10 21:27:19 +0300

    feat: add customer controller

--- apps/customer-app/src/customer/controller/customer.controller.ts
+++ apps/customer-app/src/customer/controller/customer.controller.ts
@@ -1,4 +1,43 @@
 import { Controller } from '@nestjs/common';
+import { EventPattern, RpcException } from '@nestjs/microservices';
+import {
+  CreateCustomerRequestDto,
+  CreateCustomerResponseDto,
+  UpdateCustomerStatusRequestDto,
+  UpdateCustomerStatusResponseDto,
+} from '../dto';
+import { CustomerStatus } from '../model';
+import { CustomerService } from '../service';
 
 @Controller()
-export class CustomerController {}
+export class CustomerController {
+  constructor(private readonly customerService: CustomerService) {}
+
+  @EventPattern('create-customer')
+  public async createCustomer(
+    dto: CreateCustomerRequestDto,
+  ): Promise<CreateCustomerResponseDto> {
+    try {
+      const customer = await this.customerService.createCustomer(dto);
+
+      return customer;
+    } catch (error) {
+      throw new RpcException(error.message);
+    }
+  }
+
+  @EventPattern('disable-customer')
+  public async disableCustomer(
+    dto: UpdateCustomerStatusRequestDto,
+  ): Promise<UpdateCustomerStatusResponseDto> {
+    try {
+      const data = { ...dto, status: CustomerStatus.Disabled };
+
+      const customer = await this.customerService.updateCustomerStatus(data);
+
+      return customer;
+    } catch (error) {
+      throw new RpcException(error.message);
+    }
+  }
+}
