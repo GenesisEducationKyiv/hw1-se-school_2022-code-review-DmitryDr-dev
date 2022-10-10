eae1a9d24d2f27cfe736f4694f441f73342d3b00
DmitryDr-dev <79312440+DmitryDr-dev@users.noreply.github.com>
2022-10-10 21:32:44 +0300

    feat: connect customer app to database

--- apps/customer-app/src/customer-app.module.ts
+++ apps/customer-app/src/customer-app.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { LoggerModule } from './logger/logger.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { CustomerModule } from './customer/customer.module';
+import { Customer } from './customer/model';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get<string>('PG_DB_HOST'),
+        port: configService.get<number>('PG_DB_PORT'),
+        username: configService.get<string>('PG_DB_USER'),
+        password: configService.get<string>('PG_DB_PASSWORD'),
+        database: configService.get<string>('PG_CUSTOMER_DB'),
+        models: [Customer],
+        autoLoadModels: true,
+      }),
+      inject: [ConfigService],
     }),
     LoggerModule,
     CustomerModule,
